05d66b0b537fcc5cc914bee6482eb9bf5796499b
d.uryupin <d.uryupin@stroytlm.ru>
2024-03-26 17:01:04 +0300

    update

---
+++ KAMTestStand/SetColorCell.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+
+namespace KAMTestStand;
+
+public class SetColorCell : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        switch ((ResultE)value)
+        {
+            case ResultE.StatusError:
+                return new SolidColorBrush(Colors.Red);
+            case ResultE.StatusNotStart:
+                return new SolidColorBrush(Colors.Coral);
+            case ResultE.StatusTesting:
+                return new SolidColorBrush(Colors.BlueViolet);
+            case ResultE.StatusPass:
+                return new SolidColorBrush(Colors.Green);
+            case ResultE.StatusNoAccess:
+                return new SolidColorBrush(Colors.Black);
+            case ResultE.StatusWait:
+                return new SolidColorBrush(Colors.Gray);
+
+        }
+        
+        return new SolidColorBrush(Colors.White);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return 0;
+    }
+}
